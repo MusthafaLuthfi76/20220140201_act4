7a85c02b0ebfa9859f033142ab2b6f3487ffeb95
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-04-22 22:30:26 +0700

    Menambahkan button untuk login dan register dan navigationnya

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -18,8 +18,10 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
     return Scaffold(
       body: Form(
+        key: _formKey,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -44,6 +46,26 @@ class _LoginPageState extends State<LoginPage> {
                   }
                   return null;
                 },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  }
+                },
+                child: Text('Login'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()),
+                  );
+                },
+                child: Text('Belum Punya Akun? Daftar'),
               ),
             ],
           ),
